382b3cd0190741f91423355d63343dcc392f9a2c
Eddie Dover <ed@eddiedover.dev>
2023-10-31 05:54:16 -0700

    enable vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -34,7 +34,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full w-full">
-      <body className={`w-full h-full ${inter.className}`}>{children}</body>
+      <body className={`w-full h-full ${inter.className}`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
