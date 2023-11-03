d884bc15ee7bcf1fa2793528adbe3a5307bec0da
Eddie Dover <ed@eddiedover.dev>
2023-11-03 09:45:45 -0700

    Added some lorum ipsum

--- components/NoteCraftrer.tsx
+++ components/NoteCraftrer.tsx
@@ -38,8 +38,11 @@ import { NoteImage } from "./NoteImage";
 
 interface NoteCrafterProps {}
 
+const PLACEHOLDER_TEXT =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
 export const NoteCrafter = (props: NoteCrafterProps) => {
-  const [noteText, setNoteText] = useState("This is a note");
+  const [noteText, setNoteText] = useState(PLACEHOLDER_TEXT);
   const [backgroundImage, setBackgroundImage] = useState(PAPER_LIST[0].file);
   const [customBackgroundImage, setCustomBackgroundImage] = useState<File>();
   const [fontFamily, setFontFamily] = useState("Verdana");
